513283af137a811f40d4847983823d4abe506a11
Prakhar Thakur <53122733+itsprakhar@users.noreply.github.com>
2023-05-25 13:28:47 +0530

    Added comments and fixed a bug in the demo script

--- model.py
+++ model.py
@@ -1,38 +1,61 @@
+# Importing the necessary libraries
 import torch
 import torch.nn as nn
 from torch.hub import load
 import torchvision.models as models
 
-
-# Define a custom classifier
+# Creating a class 'linear_head' that extends the nn.Module class
+# This class will act as the head of our model, meaning it's the final piece of the model which will produce predictions
 class linear_head(nn.Module):
     def __init__(self, embedding_size = 384, num_classes = 5):
         super(linear_head, self).__init__()
+        # This line is defining a fully connected (linear) layer which takes 'embedding_size' inputs and outputs 'num_classes' values
         self.fc = nn.Linear(embedding_size, num_classes)
 
+    # This is the forward method, defining the forward pass of the linear_head
     def forward(self, x):
         return self.fc(x)
 
-
+# Creating a class 'Classifier' that extends the nn.Module class
+# This will be our final classification model
 class Classifier(nn.Module):
-    def __init__(self, num_classes, backbone = 'dinov2s', head = 'linear'):
+    def __init__(self, num_classes, backbone = 'dinov2_s', head = 'linear'):
         super(Classifier, self).__init__()
+        # Defining the types of heads our model can have. Here, we have only one type: 'linear'
         self.heads = {
             'linear':linear_head
         }
+        # Defining the backbones our model can have with their corresponding names and embedding sizes
         self.backbones = {
-            'dinov2s':{
+            'dinov2_s':{
                 'name':'dinov2_vits14',
                 'embedding_size':384
-            }
+            },
+            'dinov2_b':{
+                'name':'dinov2_vitb14',
+                'embedding_size':768
+            },
+            'dinov2_l':{
+                'name':'dinov2_vitl14',
+                'embedding_size':1024
+            },
+            'dinov2_g':{
+                'name':'dinov2_vitg14',
+                'embedding_size':1536
+            },
         }
+        # Loading the backbone model using torch.hub.load
         self.backbone = load('facebookresearch/dinov2', self.backbones[backbone]['name'])
+        # Setting the backbone to evaluation mode
         self.backbone.eval()
+        # Initializing the head of the model
         self.head = self.heads[head](self.backbones[backbone]['embedding_size'],num_classes)
 
+    # This is the forward method, defining the forward pass of the Classifier
     def forward(self, x):
+        # The input first goes through the backbone. We use 'torch.no_grad()' to avoid calculating gradients during the forward pass of the backbone
         with torch.no_grad():
             x = self.backbone(x)
+        # Then it goes through the head
         x = self.head(x)
         return x
-
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -7,60 +8,68 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 import numpy as np
 import os
 from tqdm import tqdm
+# Import the Classifier class from model.py
 from model import Classifier
 
-# Dataloader
+# Define data augmentation and normalization for training
 transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.RandomResizedCrop(224),  # Randomly resize and crop the images to 224x224
+    transforms.RandomHorizontalFlip(),  # Randomly flip the images horizontally
+    transforms.ToTensor(),  # Convert the images to PyTorch tensors
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize the tensors with mean and standard deviation
 ])
 
+# Load data
 data_dir = 'data/train'
-full_dataset = datasets.ImageFolder(data_dir, transform)
-train_size = int(0.8 * len(full_dataset))  # 80% for training
+full_dataset = datasets.ImageFolder(data_dir, transform)  # Load images and labels from the directory
+train_size = int(0.8 * len(full_dataset))  # 80% of the dataset for training
 val_size = len(full_dataset) - train_size  # 20% for validation
 
+# Split the data into training and validation datasets
 train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True) #, num_workers=4, persistent_workers=True)
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False) #, num_workers=4, persistent_workers=True)
+# Create data loaders for training and validation datasets
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 dataloaders = {'train': train_loader, 'val': val_loader}
 
+# Main execution
 if __name__ == "__main__":
 
+    # Set the device to GPU if available, otherwise CPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     num_classes = len(full_dataset.classes)
+    # Initialize the model and send it to the device
     model = Classifier(num_classes).to(device)
 
-    # Define loss function, optimizer, and learning rate scheduler
+    # Define loss function (criterion), optimizer, and learning rate scheduler
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     scheduler = ReduceLROnPlateau(optimizer, 'min', patience=7, verbose=True)
 
-    # Early stopping parameters
+    # Define early stopping parameters
     n_epochs_stop = 6
     epochs_no_improve = 0
     min_val_loss = np.Inf
 
     # Training loop
-    for epoch in range(100):  # for the sake of example, we train for 100 epochs
+    for epoch in range(100):
         print('Epoch {}/{}'.format(epoch, 100 - 1))
         print('-' * 10)
 
+        # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                model.train()
+                model.train()  # Set model to training mode
             else:
-                model.eval()
+                model.eval()   # Set model to evaluate mode
 
             running_loss = 0.0
             correct = 0
             total = 0
 
-            # Iterate over data with tqdm progress bar
+            # Progress bar setup with tqdm
             with tqdm(total=len(dataloaders[phase]), unit='batch') as p:
                 for inputs, labels in dataloaders[phase]:
                     inputs = inputs.to(device)
@@ -69,18 +78,18 @@ if __name__ == "__main__":
                     # Zero the parameter gradients
                     optimizer.zero_grad()
 
-                    # Forward
+                    # Forward pass
                     with torch.set_grad_enabled(phase == 'train'):
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
-                        # Backward + optimize only if in training phase
+                        # Backward pass and optimize only in training phase
                         if phase == 'train':
                             loss.backward()
                             optimizer.step()
 
-                    # Statistics
+                    # Gather training statistics - loss and accuracy
                     running_loss += loss.item() * inputs.size(0)
                     total += labels.size(0)
                     correct += (preds == labels).sum().item()
@@ -89,24 +98,32 @@ if __name__ == "__main__":
                     p.set_postfix({'loss': loss.item(), 'accuracy': 100 * correct / total})
                     p.update(1)
 
+            # Compute the average loss and accuracy over the entire epoch
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = 100 * correct / total
 
             print('{} Loss: {:.4f} Acc: {:.2f}%'.format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
+            # If the model's validation loss is lower than our current minimum,
+            # save this model as the best model so far
             if phase == 'val':
                 if epoch_loss < min_val_loss:
                     print(f'Validation Loss Decreased({min_val_loss:.6f}--->{epoch_loss:.6f}) \t Saving The Model')
                     min_val_loss = epoch_loss
+                    # Save the model
                     torch.save(model.state_dict(), 'weights/saved_model.pt')
                     epochs_no_improve = 0
                 else:
+                    # If the validation loss didn't improve, increase the stop counter
                     epochs_no_improve += 1
-                    # Check early stopping condition
+                    # If the validation loss hasn't improved in several epochs, stop the training early
                     if epochs_no_improve == n_epochs_stop:
                         print('Early stopping!')
-                        model.load_state_dict(torch.load('weights/saved_model.pt'))  # load the last best model
+                        # Load the last best model saved
+                        model.load_state_dict(torch.load('weights/saved_model.pt')) 
                         model.eval()
                         break  # exit the loop
+        # Step the learning rate scheduler
         scheduler.step(epoch_loss)  # update learning rate scheduler
+
