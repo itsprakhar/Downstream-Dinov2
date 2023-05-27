064799a61c8cf678714a3bf2032a4d10009f4a42
Prakhar Thakur <53122733+itsprakhar@users.noreply.github.com>
2023-05-27 11:56:57 +0530

    re-organised the trin.py code

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# Importing necessary libraries
+# Import necessary modules
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,54 +6,43 @@ from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import numpy as np
-import os
 from tqdm import tqdm
-# Import the Classifier class from model.py
-from model import Classifier
+from model import Classifier  # Import custom model from model.py file
 
-# Define data augmentation and normalization for training
-transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),  # Randomly resize and crop the images to 224x224
-    transforms.RandomHorizontalFlip(),  # Randomly flip the images horizontally
-    transforms.ToTensor(),  # Convert the images to PyTorch tensors
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize the tensors with mean and standard deviation
-])
+# Define a function for loading and transforming image data
+def load_data(data_dir='data/train'):
+    # Define transformations: random crop, random flip, convert to tensor, and normalize
+    transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),  # Resize and crop the image to a 224x224 square
+        transforms.RandomHorizontalFlip(),  # Randomly flip the image horizontally
+        transforms.ToTensor(),  # Convert the image to a tensor
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize the image with mean and standard deviation
+    ])
 
-# Load data
-data_dir = 'data/train'
-full_dataset = datasets.ImageFolder(data_dir, transform)  # Load images and labels from the directory
-train_size = int(0.8 * len(full_dataset))  # 80% of the dataset for training
-val_size = len(full_dataset) - train_size  # 20% for validation
+    # Load the dataset from directory and apply transformations
+    full_dataset = datasets.ImageFolder(data_dir, transform)
 
-# Split the data into training and validation datasets
-train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    # Split the full dataset into train and validation sets
+    train_size = int(0.8 * len(full_dataset))  # 80% of the dataset is used for training
+    val_size = len(full_dataset) - train_size  # The rest is used for validation
 
-# Create data loaders for training and validation datasets
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    # Randomly split dataset into training and validation dataset
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-dataloaders = {'train': train_loader, 'val': val_loader}
+    # Create data loaders for train and validation sets
+    # They provide an easy way to iterate over the dataset in mini-batches
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)  # Shuffle the training data
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)  # No need to shuffle validation data
 
-# Main execution
-if __name__ == "__main__":
+    return {'train': train_loader, 'val': val_loader}, len(full_dataset.classes)  # Return loaders and number of classes in the dataset
 
-    # Set the device to GPU if available, otherwise CPU
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    num_classes = len(full_dataset.classes)
-    # Initialize the model and send it to the device
-    model = Classifier(num_classes).to(device)
+# Define a function to train the model
+def train_model(model, dataloaders, criterion, optimizer, scheduler, device, n_epochs_stop=6):
+    # Initialize variables
+    min_val_loss = np.Inf  # Minimum validation loss starts at infinity
+    epochs_no_improve = 0  # No improvement in epochs counter
 
-    # Define loss function (criterion), optimizer, and learning rate scheduler
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=7, verbose=True)
-
-    # Define early stopping parameters
-    n_epochs_stop = 6
-    epochs_no_improve = 0
-    min_val_loss = np.Inf
-
-    # Training loop
+    # Loop over epochs
     for epoch in range(100):
         print('Epoch {}/{}'.format(epoch, 100 - 1))
         print('-' * 10)
@@ -63,67 +52,85 @@ if __name__ == "__main__":
             if phase == 'train':
                 model.train()  # Set model to training mode
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()  # Set model to evaluate mode
 
-            running_loss = 0.0
-            correct = 0
-            total = 0
+            # Initialize metrics for this phase
+            running_loss = 0.0  # Accumulate losses over the epoch
+            correct = 0  # Count correct predictions
+            total = 0  # Count total predictions
 
-            # Progress bar setup with tqdm
+            # Use tqdm for progress bar
             with tqdm(total=len(dataloaders[phase]), unit='batch') as p:
+                # Iterate over mini-batches
                 for inputs, labels in dataloaders[phase]:
+                    # Move input and label tensors to the default device (GPU or CPU)
                     inputs = inputs.to(device)
                     labels = labels.to(device)
 
-                    # Zero the parameter gradients
+                    # Clear the gradients of all optimized variables
                     optimizer.zero_grad()
 
-                    # Forward pass
-                    with torch.set_grad_enabled(phase == 'train'):
+                    # Forward pass: compute predicted outputs by passing inputs to the model
+                    with torch.set_grad_enabled(phase == 'train'):  # Only calculate gradients in training phase
                         outputs = model(inputs)
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
+                        _, preds = torch.max(outputs, 1)  # Get the class with the highest probability
+                        loss = criterion(outputs, labels)  # Compute the loss
 
-                        # Backward pass and optimize only in training phase
+                        # Perform backward pass and optimization only in the training phase
                         if phase == 'train':
-                            loss.backward()
-                            optimizer.step()
+                            loss.backward()  # Calculate gradients based on the loss
+                            optimizer.step()  # Update model parameters based on the current gradient
 
-                    # Gather training statistics - loss and accuracy
-                    running_loss += loss.item() * inputs.size(0)
+                    # Update running loss and correct prediction count
+                    running_loss += loss.item() * inputs.size(0)  # Multiply average loss by batch size
                     total += labels.size(0)
-                    correct += (preds == labels).sum().item()
+                    correct += (preds == labels).sum().item()  # Update correct predictions count
 
-                    # Update tqdm progress bar
+                    # Update the progress bar
                     p.set_postfix({'loss': loss.item(), 'accuracy': 100 * correct / total})
                     p.update(1)
 
-            # Compute the average loss and accuracy over the entire epoch
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = 100 * correct / total
+                # Calculate loss and accuracy for this epoch
+                epoch_loss = running_loss / len(dataloaders[phase].dataset)
+                epoch_acc = 100 * correct / total
 
-            print('{} Loss: {:.4f} Acc: {:.2f}%'.format(phase, epoch_loss, epoch_acc))
+                print('{} Loss: {:.4f} Acc: {:.2f}%'.format(phase, epoch_loss, epoch_acc))
 
-            # deep copy the model
-            # If the model's validation loss is lower than our current minimum,
-            # save this model as the best model so far
-            if phase == 'val':
-                if epoch_loss < min_val_loss:
-                    print(f'Validation Loss Decreased({min_val_loss:.6f}--->{epoch_loss:.6f}) \t Saving The Model')
-                    min_val_loss = epoch_loss
-                    # Save the model
-                    torch.save(model.state_dict(), 'weights/saved_model.pt')
-                    epochs_no_improve = 0
-                else:
-                    # If the validation loss didn't improve, increase the stop counter
-                    epochs_no_improve += 1
-                    # If the validation loss hasn't improved in several epochs, stop the training early
-                    if epochs_no_improve == n_epochs_stop:
-                        print('Early stopping!')
-                        # Load the last best model saved
-                        model.load_state_dict(torch.load('weights/saved_model.pt')) 
-                        model.eval()
-                        break  # exit the loop
-        # Step the learning rate scheduler
-        scheduler.step(epoch_loss)  # update learning rate scheduler
+                # Save model and implement early stopping in validation phase
+                if phase == 'val':
+                    if epoch_loss < min_val_loss:
+                        print(f'Validation Loss Decreased({min_val_loss:.6f}--->{epoch_loss:.6f}) \t Saving The Model')
+                        min_val_loss = epoch_loss  # Update minimum validation loss
+                        torch.save(model.state_dict(), 'weights/saved_model.pt')  # Save the current model weights
+                        epochs_no_improve = 0  # Reset epochs since last improvement
+                    else:
+                        epochs_no_improve += 1
+                        # Implement early stopping
+                        if epochs_no_improve == n_epochs_stop:
+                            print('Early stopping!')
+                            model.load_state_dict(torch.load('weights/saved_model.pt'))  # Load the best model weights
+                            model.eval()
+                            return model  # Exit the function early
 
+            # Adjust the learning rate based on the scheduler
+            scheduler.step(epoch_loss)
+
+    # Return the trained model
+    return model
+
+
+if __name__ == "__main__":
+    # Decide whether to use a GPU or not
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Load data
+    dataloaders, num_classes = load_data()
+
+    # Initialize model, loss function (criterion), optimizer, and learning rate scheduler
+    model = Classifier(num_classes).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=7, verbose=True)
+
+    # Train the model
+    model = train_model(model, dataloaders, criterion, optimizer, scheduler, device)
